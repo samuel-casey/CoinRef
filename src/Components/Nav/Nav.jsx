09721184be7de783447df61d32b2f7ce6e824f6a
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-10 10:53:10 -0400

    added links to Nav component

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,5 +3,19 @@ import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 export const Nav = () => {
-	return <nav>Nav</nav>;
+	return (
+		<nav>
+			<Link to='/'>
+				<h4>Learn</h4>
+			</Link>
+
+			<Link to='/news'>
+				<h4>News</h4>
+			</Link>
+
+			<Link to='/about'>
+				<h4>About</h4>
+			</Link>
+		</nav>
+	);
 };
