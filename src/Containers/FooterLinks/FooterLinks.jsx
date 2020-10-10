e9529ba753cdc0e16cebfab89c7055454d746ef4
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-10 11:20:06 -0400

    added content to FooterLinks

--- src/Containers/FooterLinks/FooterLinks.jsx
+++ src/Containers/FooterLinks/FooterLinks.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import './FooterLinks.scss';
 
 export const FooterLinks = (props) => {
-	return <div>FooterLinks</div>;
+	return (
+		<div id='footer-links'>
+			<div id='disclaimer'>
+				All content on this site is intended to be educational and should not be
+				perceived as financial advice. Data from
+				<span>
+					<a href='https://messari.io' target='blank'>
+						{' '}
+						messari.io.
+					</a>
+				</span>
+			</div>
+			<ul id='links'>
+				<li>
+					<a href='https://github.com/samuel-casey/coinref' target='blank'>
+						GitHub
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
