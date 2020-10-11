ce51500f3048b01c64d99713bd51b887bc18e1be
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-11 17:50:57 -0400

    added data to newsProviderList

--- src/Components/NewsProvider/NewsProvider.jsx
+++ src/Components/NewsProvider/NewsProvider.jsx
@@ -5,8 +5,8 @@ export const NewsProvider = ({ name, image, url }) => {
 	return (
 		<div className='news-provider'>
 			<a href={url} target='blank'>
+				<img src={image} alt={`${name}-img`} />
 				<h6>{name}</h6>
-				<img src={image} alt='name-image' />
 			</a>
 		</div>
 	);
--- src/newsProviders.js
+++ src/newsProviders.js
@@ -11,10 +11,21 @@ export const newsProviders = [
 			'https://res.cloudinary.com/scimgcloud/image/upload/v1602449899/CoinRef/coindesk-png_c0am7r.png',
 	},
 	{
+		name: 'The Block',
+		url: 'https://www.theblockcrypto.com/',
+		image:
+			'https://www.theblockcrypto.com/dist/img/tb-logo-2020-w.7a1aae0d.svg',
+	},
+	{
 		name: 'Coin Journal',
 		url: 'https://coinjournal.net/',
 		image:
 			'https://res.cloudinary.com/scimgcloud/image/upload/v1602450297/CoinRef/Screen_Shot_2020-10-11_at_5.04.38_PM_wwdt6b.png',
 	},
-	{ name: 'Coin Telegraph', url: '', image: '' },
+	{
+		name: 'Coin Telegraph',
+		url: 'https://cointelegraph.com/',
+		image:
+			'https://res.cloudinary.com/scimgcloud/image/upload/v1602452649/CoinRef/coinTelegraph.png',
+	},
 ];
