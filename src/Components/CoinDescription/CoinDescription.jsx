da39bb126c87964207151d1ef35c20d1631b2cfd
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-10 21:12:22 -0400

    copied initial description-fetching code from code-sandbox

--- src/Components/CoinDescription/CoinDescription.jsx
+++ src/Components/CoinDescription/CoinDescription.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './CoinDescription.scss';
 
 export const CoinDescription = (props) => {
+
+const [currentCoin, setCurrentCoin] = useState("btc");
+  const [coinProfileData, setCoinProfileData] = useState(null);
+
+  function fetchAssetData(coin) {
+    fetch(`https://data.messari.io/api/v2/assets/${coin}/profile`, {
+      headers: {
+        method: "GET",
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((body) => {
+        const data = body.data;
+        setCoinProfileData(data);
+      })
+      .catch((error) => console.log(error.message));
+  }
+
+  // logging twice
+  useEffect(() => {
+    fetchAssetData(currentCoin);
+  }, []);
+
+  let description;
+
+    coinProfileData
+      ? (description = coinProfileData.profile.general.overview.project_details.replace(
+          /<([^>]+)>/g,
+          ""
+        ))
+      : `Loading data for ${currentCoin}`;
+  }
 	return (
 		<div className='coin-description'>
 			<h5>bitcoin</h5>
