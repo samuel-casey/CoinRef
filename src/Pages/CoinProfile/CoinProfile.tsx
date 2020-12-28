ca50f96b453aa3c29233c3a3c6bc822d9b397043
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-12-28 11:56:55 -0500

    all functionality working for CoinProfile

--- src/Pages/CoinProfile/CoinProfile.tsx
+++ src/Pages/CoinProfile/CoinProfile.tsx
@@ -8,6 +8,7 @@ import { Store } from '../../Store';
 import { CoinChart } from '../../Components/CoinChart/CoinChart';
 import { setChartDataInterval } from '../../helpers/dateHelpers';
 import { fetchAssetMetricsData, fetchAssetPriceData, fetchAssetProfileData } from '../../apis/messari';
+import PriceLinePoint from '../../PriceLinePoint';
 
 const { REACT_APP_MESSARI_API_KEY } = process.env;
 
@@ -16,7 +17,7 @@ export const CoinProfile = (): JSX.Element => {
 	const { currentCoin, errorMsg } = gState;
 	const [coinProfileData, setCoinProfileData] = useState(null);
 	const [coinMetricsData, setCoinMetricsData] = useState(null);
-	const [chartData, setChartData] = useState();
+	const [chartData, setChartData] = useState<Array<PriceLinePoint>>();
 	const [numDaysPriceData, setNumDaysPriceData] = useState(30)
 
 	// calc today's date and num days in the past to get data for based on user input
@@ -30,7 +31,7 @@ export const CoinProfile = (): JSX.Element => {
 			const priceData = await fetchAssetPriceData(currentCoin, gState, dispatch, today, maxInterval)
 			setCoinProfileData(profile)
 			setCoinMetricsData(metrics)
-			// setChartData(priceData)
+			setChartData(priceData)
 		}
 
 		getCoinData();
